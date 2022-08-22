866eab9448a4e76adf94cc2a907428cdbb544fad
Amit Vikram <amvi19cs@cmrit.ac.in>
2022-08-22 21:46:24 +0530

    deploying

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+
 app.post("/register",async (req, resp) => {
     let user = new User(req.body);
     let result = await user.save();
